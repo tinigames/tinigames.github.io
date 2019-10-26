8d2802413b9b9b1645d7d320d30ea2012fa665e1
nguyenhau07 <ziczac.nack@gmail.com>
2019-10-26 11:58:10 +0700

    +games

---
+++ games/kai/sc/sw.js
@@ -0,0 +1,9 @@
+/*
+	This is a service worker file, but is not used. It exists solely to trigger
+	the Web App install banner in Chrome, which requires there to be a service worker
+	registered. See: http://updates.html5rocks.com/2015/03/increasing-engagement-with-app-install-banners-in-chrome-for-android
+*/
+
+self.addEventListener("fetch", event => {
+	// dummy
+});
